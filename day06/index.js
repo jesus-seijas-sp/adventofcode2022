1e1c902f7dd046bbdaf34584135391593215bc00
Jesus Seijas <jseijas@gmail.com>
2022-12-06 14:21:10 +0900

    minor fixes day06

--- day06/index.js
+++ day06/index.js
@@ -1,11 +1,11 @@
 const { loadTxt } = require('../utils');
 
 const input = loadTxt('./input.txt');
-function indexDifferent(input, amount) {
-  for (let i = amount; i < input.length; i += 1) {
-    if ((new Set(input.substring(i - amount, i))).size === amount)
+const indexDifferent = (str, length) => {
+  for (let i = length; i < input.length; i += 1) {
+    if ((new Set(str.substring(i - length, i))).size === length)
       return i;
   }
 }
-console.log(`Part1: ${indexDifferent(input, 4)}`)
-console.log(`Part2: ${indexDifferent(input, 14)}`)
+console.log(`Part1: ${indexDifferent(input, 4)}`);
+console.log(`Part2: ${indexDifferent(input, 14)}`);
