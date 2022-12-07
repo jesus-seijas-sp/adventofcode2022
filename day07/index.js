872cbd9718b572bd1718754b3bf10b473f5953a9
Jesus Seijas <jseijas@gmail.com>
2022-12-07 14:53:13 +0900

    day07

--- day07/index.js
+++ day07/index.js
@@ -1,30 +1,24 @@
-const { join } = require('path');
+const { join, sep } = require('path');
 const { loadLines, sum, sort } = require('../utils');
 
 const lines = loadLines('./input.txt');
 const paths = {};
 let currentPath = '';
-const root = join('/', '');
-
 const incSize = (current, size) => {
   paths[current] += size;
-  if (current !== root) {
+  if (current !== sep) {
     incSize(join(current, '..'), size);
   }
 };
-
 lines.forEach((line) => {
   if (line.startsWith('$ cd')) {
     currentPath = join(currentPath, line.slice(5));
-    if (!paths[currentPath]) {
-      paths[currentPath] = 0;
-    }
+    paths[currentPath] ??= 0;
   } else if (!line.startsWith('$') && !line.startsWith('dir')) {
-    const size = parseInt(line.split(' ')[0]);
-    incSize(currentPath, size);
+    incSize(currentPath, parseInt(line.split(' ')[0]));
   }
 });
 const sizes = Object.values(paths);
 console.log(`Part 1: ${sum(sizes.filter(x => x <= 100000))}`)
-const minValue = paths[root] - 40000000;
+const minValue = paths[sep] - 40000000;
 console.log(`Part 2: ${sort(sizes.filter(x => x >= minValue))[0]}`);
