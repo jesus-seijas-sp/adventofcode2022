a2d0db71c10c85d92c94037b7e45998b4515d1af
Jesus Seijas <jseijas@gmail.com>
2022-12-16 15:09:29 +0100

    day16

--- day16/index.js
+++ day16/index.js
@@ -1,32 +1,18 @@
 const { loadLines } = require('../utils');
 
-const exp = /Valve (..) has flow rate=(.*); tunnels? leads? to valves? (.*)/;
 const valves = loadLines('./input.txt')
-  .map((line) => exp.exec(line).slice(1))
-  .reduce((p, c) => {
-    p[c[0]] = {
-      name: c[0],
-      rate: Number(c[1]),
-      childs: c[2].split(', '),
-    }
-    return p;
-  }, {});
+  .map((line) => /Valve (..) has flow rate=(\d+); tunnels? leads? to valves? (.*)/.exec(line).slice(1))
+  .reduce((p, c) => (p[c[0]] = { id: c[0], rate: Number(c[1]), childs: c[2].split(', ') }, p), {});
 
 const distances = Object.keys(valves).reduce((p, node) => {
-    p[node] = {};
-    let nodes = [node];
-    let distance = 0
-    while (nodes.length > 0) {
-      const nextNodes = [];
-      nodes.forEach(n => {
-        if (p[node][n] === undefined) {
-          p[node][n] = distance;
-          nextNodes.push(...valves[n].childs);
-        }
-      })
-      nodes = nextNodes;
-      distance += 1;
-    }
+  p[node] = { };
+  for (let distance = 1, nodes = [...valves[node].childs]; nodes.length; distance += 1) {
+    nodes = nodes.filter(n => !p[node][n]).reduce((q, n) => {
+      p[node][n] = distance;
+      q.push(...valves[n].childs);
+      return q;
+    }, []);
+  }
   return p;
 }, {});
 
@@ -34,12 +20,12 @@ const getPaths = (flows) => flows.map(x => ({ next: x, flows: flows.filter(c => 
 
 function solve(start, currentFlows, minsLeft, cb) {
   const best = getPaths(currentFlows).reduce((p, { next, flows }) => {
-    const t = minsLeft - distances[start][next.name] - 1;
-    return t < 0 ? p : Math.max(p, next.rate * t + solve(next.name, flows, t, cb));
+    const t = minsLeft - distances[start][next.id] - 1;
+    return t < 0 ? p : Math.max(p, next.rate * t + solve(next.id, flows, t, cb));
   }, 0);
   return cb ? cb(best, currentFlows) : best;
 }
 
 const goodValves = Object.values(valves).filter(v => v.rate);
-console.log(solve('AA', goodValves , 30));
-console.log(solve('AA', goodValves, 26, (b, f) => Math.max(b, solve('AA', f, 26))));
+console.log(`Part 1: ${solve('AA', goodValves, 30)}`);
+console.log(`Part 2: ${solve('AA', goodValves, 26, (b, f) => Math.max(b, solve('AA', f, 26)))}`);
